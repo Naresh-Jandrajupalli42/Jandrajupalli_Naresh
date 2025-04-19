30bda747ca2cf124a20a386b2a196f44b9bcbeb1
Naresh-Jandrajupalli42 <nareshjandrajupalli42@gmail.com>
2025-04-19 16:04:09 +0530

    Problem-2.py

--- Problem-2.py
+++ Problem-2.py
@@ -1,1 +1,13 @@
+class OddNumberSeries:
+    def generate_series(self, a):
+        series = []
+        for i in range(a):
+            series.append(2 * i + 1)
+        return ",".join(map(str, series))
 
+# Taking user input
+a = int(input("Enter an integer: "))
+
+# Create instance and generate series
+odd_series = OddNumberSeries()
+print(odd_series.generate_series(a))
